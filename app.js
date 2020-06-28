dcace7351cf8b1078b8057997ba1c935732c5a74
Danyllo Silva² <danyllo.rsilva@gmail.com>
2020-06-27 22:34:10 -0300

    EasyLinks v1.0

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-chrome.tabs.query({ currentWindow:true, highlighted : true }, function(tab) {
-	$('#btCxOne').hover(
-		function(){
-			$(this).css('background', 'red');
-		},
-		function(){
-			$(this).css('background','white');
-		}
-
-});
+chrome.tabs.query({ currentWindow:true, highlighted : true }, function(tab) {});
